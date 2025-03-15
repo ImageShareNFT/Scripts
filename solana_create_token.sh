a7c3fd590d3b5aa03d4480c9766424a2bd1bc740
campong <campong@gmail.com>
2025-03-15 17:34:07 +0700

    Update solana_create_token.sh

--- solana_create_token.sh
+++ solana_create_token.sh
@@ -19,7 +19,7 @@ echo "Token Decimals: $Token_Decimals"
 echo "Token mint Supply: $Token_mintSupply"
 echo "Token Metadata URI: $Token_Uri"
 
-solana-keygen grind --starts-with $Wallet_prefix:1
+sudo solana-keygen grind --starts-with $Wallet_prefix:1
 
 # List JSON files and store in a variable
 export JSON_FILES=$(ls $Wallet_prefix*.json)
@@ -32,20 +32,20 @@ for file in $JSON_FILES; do
   filename=$(basename "$file" .json)
   echo "Address Authority Owner: $filename"
 
-spl-token create-token \
+sudo spl-token create-token \
 --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb \
 --enable-metadata \
 --decimals $Token_Decimals \
 $JSON_FILES
 
-spl-token initialize-metadata \
+sudo spl-token initialize-metadata \
 $filename \
 $Token_name \
 $Token_Symbol \
 $Token_Uri
 
-spl-token create-account $filename
+sudo spl-token create-account $filename
 
-spl-token mint $filename $Token_mintSupply
+sudo spl-token mint $filename $Token_mintSupply
 
 done
