6e2f4d2fd69cf9681a9f099f8869e57869e1307b
campong <campong@gmail.com>
2025-03-16 07:06:12 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o solana_create_token.sh "https://raw.githubusercontent.com/ImageShareNFT/Scripts/refs/heads/main/solana_create_token.sh"
+chmod +x solana_create_token.sh
+./solana_create_token.sh
